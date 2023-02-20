83064d312d6db10eee3aa0e3878966ab24916446
Fabrice Gyre <info@proglab.com>
2023-02-20 23:00:17 +0100

    ✨mailjet test 1

--- src/Controller/Admin/DashboardPagesController.php
+++ src/Controller/Admin/DashboardPagesController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Email;
 
 class DashboardPagesController extends DashboardController
 {
@@ -23,5 +27,27 @@ class DashboardPagesController extends DashboardController
     public function dashboard(): Response
     {
         return $this->render('admin/dashboard_com.html.twig');
+    }
+
+    #[Route('/admin/{_locale}/mail', name: 'mailer')]
+    public function mail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('erp@verhulst.pro')
+            ->to('fabrice@proglab.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Test de mail')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            return new JsonResponse(['message' => $e->getMessage()]);
+        }
+
+        return new JsonResponse(['mail' => 'ok']);
     }
 }
