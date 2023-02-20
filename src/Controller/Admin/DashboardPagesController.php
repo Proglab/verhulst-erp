35a24f849f3abf8fe808090d9c66fe76354f588b
Fabrice Gyre <info@proglab.com>
2023-02-20 23:55:54 +0100

    ✨mailjet test 2

--- src/Controller/Admin/DashboardPagesController.php
+++ src/Controller/Admin/DashboardPagesController.php
@@ -32,22 +32,42 @@ class DashboardPagesController extends DashboardController
     #[Route('/admin/{_locale}/mail', name: 'mailer')]
     public function mail(MailerInterface $mailer): Response
     {
-        $email = (new Email())
-            ->from('erp@verhulst.pro')
-            ->to('fabrice@proglab.com')
-            //->cc('cc@example.com')
-            //->bcc('bcc@example.com')
-            //->replyTo('fabien@example.com')
-            //->priority(Email::PRIORITY_HIGH)
-            ->subject('Test de mail')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
-        try {
-            $mailer->send($email);
-        } catch (TransportExceptionInterface $e) {
-            return new JsonResponse(['message' => $e->getMessage()]);
-        }
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "erp@verhulst.pro",
+                        'Name' => "verhulst.pro"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => "info@proglab.com",
+                            'Name' => "Fabrice"
+                        ]
+                    ],
+                    'Subject' => "Greetings from Mailjet.",
+                    'HTMLPart' => "<h3>Dear User, welcome to Mailjet!</h3><br />May the delivery force be with you!"
+                ]
+            ]
+        ];
 
-        return new JsonResponse(['mail' => 'ok']);
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, "https://api.mailjet.com/v3.1/send");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json')
+        );
+        curl_setopt($ch, CURLOPT_USERPWD, "669e4d298a8a40cd24523ab3bc3c598b:b92be50960a11ff4c03f6bbab91945b3");
+        $server_output = curl_exec($ch);
+        curl_close ($ch);
+
+        $response = json_decode($server_output);
+        dd($response);
+        if ($response->Messages[0]->Status == 'success') {
+            echo "Email sent successfully.";
+        }
     }
 }
