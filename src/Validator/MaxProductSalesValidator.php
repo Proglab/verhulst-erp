7e8cc64588232829dbf45ad25e40baa581a65703
Fabrice Gyre <info@proglab.com>
2023-03-14 14:07:00 +0100

    :bug: max product on update sales

--- src/Validator/MaxProductSalesValidator.php
+++ src/Validator/MaxProductSalesValidator.php
@@ -8,12 +8,13 @@ use App\Entity\ProductPackageVip;
 use App\Entity\ProductSponsoring;
 use App\Entity\Sales;
 use App\Repository\SalesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class MaxProductSalesValidator extends ConstraintValidator
 {
-    public function __construct(private SalesRepository $salesRepository)
+    public function __construct(private SalesRepository $salesRepository, private EntityManagerInterface $entityManager)
     {
     }
 
@@ -38,11 +39,34 @@ class MaxProductSalesValidator extends ConstraintValidator
         }
 
         $quantity_sales = $this->salesRepository->getQuantitySaleByProduct($product);
-        if (null !== $product->getQuantityMax() && $quantity_sales + $sales->getQuantity() > $product->getQuantityMax()) {
+
+        if ($sales->getId() === null && null !== $product->getQuantityMax() && $quantity_sales + $sales->getQuantity() > $product->getQuantityMax()) {
             $this->context
                 ->buildViolation(sprintf($constraint->maxSalesExceededMessage, $quantity_sales, $product->getQuantityMax()))
                 ->atPath('Sales.quantity')
                 ->addViolation();
         }
+
+        if ($sales->getId() !== null) {
+            $newSales = clone $sales;
+            $this->entityManager->refresh($sales);
+            if (null !== $product->getQuantityMax() && $quantity_sales - $sales->getQuantity() + $newSales->getQuantity() > $product->getQuantityMax()) {
+                $this->context
+                    ->buildViolation(sprintf($constraint->maxSalesExceededMessage, $quantity_sales - $sales->getQuantity(), $product->getQuantityMax()))
+                    ->atPath('Sales.quantity')
+                    ->addViolation();
+            }
+
+            $sales->setProduct($newSales->getProduct());
+            $sales->setContact($newSales->getContact());
+            $sales->setQuantity($newSales->getQuantity());
+            $sales->setPrice($newSales->getPrice());
+            $sales->setDiscount($newSales->getDiscount());
+            $sales->setPercentCom($newSales->getPercentCom());
+            $sales->setPercentVr($newSales->getPercentVr());
+            $sales->setDate($newSales->getDate());
+
+        }
+
     }
 }
