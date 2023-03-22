e9064663e33e68f73a7fb9f61af2e9ac708265f4
Fabrice Gyre <info@proglab.com>
2023-03-22 11:11:32 +0100

    :recycle: phpStan & php cs fixer

--- src/Security/Voter/ProductVoter.php
+++ src/Security/Voter/ProductVoter.php
@@ -1,23 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Security\Voter;
 
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Bundle\SecurityBundle\Security;
 
 class ProductVoter extends Voter
 {
     public const CAN_ADD_PRODUCT = 'CAN_ADD_PRODUCT';
 
-    public function __construct(private Security $security) {
-
+    public function __construct(private Security $security)
+    {
     }
 
     protected function supports(string $attribute, mixed $subject): bool
     {
-        return in_array($attribute, [self::CAN_ADD_PRODUCT]);
+        return \in_array($attribute, [self::CAN_ADD_PRODUCT], true);
     }
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
