22514dcd9a7d0f4691821a41e0d7e2f230e808c7
Fabrice Gyre <info@proglab.com>
2023-03-24 12:04:11 +0100

    ✨ toogle menu

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,21 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+$('#sidebar-toogle')
+    .click(function() {
+        var content = $('#sidebar-toogle').html();
+        if ('<i class="fa-solid fa-arrow-right"></i>' == content) {
+            $('#sidebar-toogle').html('<i class="fa-solid fa-arrow-left"></i>');
+        } else {
+            $('#sidebar-toogle').html('<i class="fa-solid fa-arrow-right"></i>');
+        }
+        $('#sidebar-resizer-handler').trigger( "click" );
+    });
+
+console.log($('body').hasClass('ea-sidebar-width-normal'))
+if ($('body').hasClass('ea-sidebar-width-normal')) {
+    $('#sidebar-toogle').html('<i class="fa-solid fa-arrow-left"></i>');
+} else {
+    $('#sidebar-toogle').html('<i class="fa-solid fa-arrow-right"></i>');
+}
