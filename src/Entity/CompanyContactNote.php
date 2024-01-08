e14efaf8c2548ccdf046cc77ebe00ddb7e1edcb0
Fabrice <info@proglab.com>
2024-01-08 14:10:09 +0100

    ✨notes (#21)

---
+++ src/Entity/CompanyContactNote.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CompanyContactNoteRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CompanyContactNoteRepository::class)]
+class CompanyContactNote
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $note = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $created_dt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'notes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CompanyContact $company_contact = null;
+
+    #[ORM\ManyToOne(inversedBy: 'notes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getCreatedDt(): ?\DateTimeInterface
+    {
+        return $this->created_dt;
+    }
+
+    public function setCreatedDt(\DateTimeInterface $created_dt): static
+    {
+        $this->created_dt = $created_dt;
+
+        return $this;
+    }
+
+    public function getCompanyContact(): ?CompanyContact
+    {
+        return $this->company_contact;
+    }
+
+    public function setCompanyContact(?CompanyContact $company_contact): static
+    {
+        $this->company_contact = $company_contact;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
---
+++ src/Repository/CompanyContactNoteRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CompanyContactNote;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<CompanyContactNote>
+ *
+ * @method CompanyContactNote|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CompanyContactNote|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CompanyContactNote[]    findAll()
+ * @method CompanyContactNote[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CompanyContactNoteRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CompanyContactNote::class);
+    }
+
+//    /**
+//     * @return CompanyContactNote[] Returns an array of CompanyContactNote objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('c.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?CompanyContactNote
+//    {
+//        return $this->createQueryBuilder('c')
+//            ->andWhere('c.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
