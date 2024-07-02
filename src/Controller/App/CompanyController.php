a291159ae3cdbc4592297c6bb557588d0408d63f
Fabrice Gyre <info@proglab.com>
2024-07-02 15:10:49 +0200

    ✨companies - part 2

--- src/Controller/App/CompanyController.php
+++ src/Controller/App/CompanyController.php
@@ -6,6 +6,7 @@ namespace App\Controller\App;
 
 use App\Entity\Project;
 use App\Entity\User;
+use App\Repository\CompanyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +16,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted(User::ROLE_USER)]
 class CompanyController extends AbstractController
 {
-    public function __construct()
+    public function __construct(private CompanyRepository $companyRepository)
     {
     }
 
@@ -24,6 +25,17 @@ class CompanyController extends AbstractController
     {
         return $this->render('app/company/index.html.twig', [
             'locale' => $_locale,
+        ]);
+    }
+
+
+    #[Route('/app/{_locale}/company/{id}/details', name: 'company_details')]
+    public function detail(string $_locale, int $id): Response
+    {
+        $company = $this->companyRepository->find($id);
+        return $this->render('app/company/details.html.twig', [
+            'locale' => $_locale,
+            'company' => $company,
         ]);
     }
 }
---
+++ src/Twig/Components/Company/CompanyDetail.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig\Components\Company;
+
+use App\Entity\Company;
+use App\Entity\CompanyContact;
+use App\Repository\CompanyContactRepository;
+use App\Repository\CompanyRepository;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('company-details', template: 'app/company/component_detail.html.twig')]
+class CompanyDetail
+{
+    use DefaultActionTrait;
+
+    #[LiveProp(writable: true)]
+    public ?Company $company = null;
+
+    #[LiveProp(writable: true)]
+    public ?CompanyContact $contact = null;
+
+    public function __construct(
+        private CompanyRepository $companyRepository,
+        private CompanyContactRepository $companyContactRepository,
+    ) {
+    }
+
+
+    #[LiveAction]
+    public function viewContact(#[LiveArg] int $contact): void
+    {
+        $this->contact = $this->companyContactRepository->find($contact);
+    }
+}
